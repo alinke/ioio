24678c2788f489164bba1138f6d3003ece4ee662
Al Linke <alinke@yahoo.com>
2015-09-06 00:51:07 -0700

    changed android BT lib to check for the bluetooth name: PIXEL or C.A.T

--- software/IOIOLibBT/src/ioio/lib/android/bluetooth/BluetoothIOIOConnectionBootstrap.java
+++ software/IOIOLibBT/src/ioio/lib/android/bluetooth/BluetoothIOIOConnectionBootstrap.java
@@ -64,7 +64,7 @@ public class BluetoothIOIOConnectionBootstrap implements
 		try {
 			Set<BluetoothDevice> bondedDevices = adapter_.getBondedDevices();
 			for (final BluetoothDevice device : bondedDevices) {
-				if (device.getName().startsWith("PIXEL")) {
+				if (device.getName().startsWith("PIXEL") || device.getName().startsWith("C.A.T")) { //add for C.A.T purse
 					result.add(new IOIOConnectionFactory() {
 						@Override
 						public String getType() {
